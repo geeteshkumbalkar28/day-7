925ef3775fd52fe36fcd19239ff06f989eb8f56c
geeteshkumbalkar028 <geeteshkumbalkar@gmail.com>
2023-01-08 15:38:08 +0530

    modified : Generates 10 Random 3 Digit number1

--- SmallestSortPrime.sh
+++ SmallestSortPrime.sh
@@ -1,1 +1,18 @@
-welcome
+#!/bin/bash -x
+
+
+
+
+RenCheck1=$(($((RANDOM%900))+100));
+RenCheck2=$(($((RANDOM%900))+100));
+RenCheck3=$(($((RANDOM%900))+100));
+RenCheck4=$(($((RANDOM%900))+100));
+RenCheck5=$(($((RANDOM%900))+100));
+RenCheck6=$(($((RANDOM%900))+100));
+RenCheck7=$(($((RANDOM%900))+100));
+RenCheck8=$(($((RANDOM%900))+100));
+RenCheck9=$(($((RANDOM%900))+100));
+RenCheck10=$(($((RANDOM%900))+100));
+
+
+
