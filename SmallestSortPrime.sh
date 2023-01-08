ebd535e51aafa3025eaf3d7f60fd07d469cca168
geeteshkumbalkar028 <geeteshkumbalkar@gmail.com>
2023-01-08 16:33:31 +0530

    modified : Store this random numbers into a array

--- SmallestSortPrime.sh
+++ SmallestSortPrime.sh
@@ -1,18 +1,9 @@
 #!/bin/bash -x
+#b. Store this random numbers into a array.
 
+for (( cnt=0; cnt<10; cnt=$((cnt+1))))
+do
+        Random_Values[$cnt]=$(($((RANDOM%900))+100));
 
-
-
-RenCheck1=$(($((RANDOM%900))+100));
-RenCheck2=$(($((RANDOM%900))+100));
-RenCheck3=$(($((RANDOM%900))+100));
-RenCheck4=$(($((RANDOM%900))+100));
-RenCheck5=$(($((RANDOM%900))+100));
-RenCheck6=$(($((RANDOM%900))+100));
-RenCheck7=$(($((RANDOM%900))+100));
-RenCheck8=$(($((RANDOM%900))+100));
-RenCheck9=$(($((RANDOM%900))+100));
-RenCheck10=$(($((RANDOM%900))+100));
-
-
+done
 
