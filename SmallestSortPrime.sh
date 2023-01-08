4dabfdf4ee1eb047b5b1c33e1c63e95796625d58
geeteshkumbalkar028 <geeteshkumbalkar@gmail.com>
2023-01-09 04:00:31 +0530

    Modified : Display the Prime Factors

--- SmallestSortPrime.sh
+++ SmallestSortPrime.sh
@@ -1,11 +1,23 @@
 #!/bin/bash -x
-#Extend the above program to sort the array and then find the 2nd largest
-#and the 2nd smallest element.
+#Extend the Prime Factorization Program to store all the Prime Factors of a
+#number n into an array and finally display the output.
 
+#this loop for add the random element and  prime factor in array
+Pr_Cnt=0
+prime=0
 for (( cnt=0; $cnt<10; cnt=$((cnt+1))))
 do
-        Random_Values[$cnt]=$(($((RANDOM%900))+100));
-	
+        
+	Random_Values[$cnt]=$(($((RANDOM%900))+100));
+	prime=${Random_Values[$cnt]}
+
+	if(( $((prime%2))!=0 ))
+        then
+
+                Prime_No[$Pr_Cnt]=${Random_Values[$cnt]}
+                Pr_Cnt=$((Pr_Cnt+1))
+        fi
+
 done
 echo "Array without sort"
 echo ${Random_Values[*]}
@@ -13,6 +25,8 @@ echo ${Random_Values[*]}
 Max=${Random_Values[0]}
 Min=${Random_Values[0]}
 
+
+#this array for find the maximum element into the array
 for (( Cnt=0; $Cnt<10; Cnt=$((Cnt+1))))
 do 
 	if (( $Max<${Random_Values[$Cnt]} )) 
@@ -48,6 +62,7 @@ echo ${Random_Values[*]}
 
 Max2=$Min
 Min2=$Max
+#this loop for fine the 2nd largest and smallest element into the array
 for (( CNt=0; $CNt<10; CNt=$((CNt+1))))
 do
         if (( $Max2<${Random_Values[$CNt]} && $Max!=${Random_Values[$CNt]} ))
@@ -60,6 +75,9 @@ do
         fi
 
 done
+
+echo "Prime no is:"
+echo ${Prime_No[*]}
 
 echo "the 2nd largest no is : $Max2"
 echo "the 2nd smallest no is : $Min2"
